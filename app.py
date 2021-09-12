a0802df976387506e7d34666116b00684ce6bb83
anildhage <i.am.dhage@gmail.com>
2021-09-12 23:46:36 +0530

    pickle files code corrections.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask,render_template,url_for,request
-import pandas as pd 
+import pandas as pd
 
 import pickle
 
 # load the model from disk
-loaded_model=pickle.load(open('decision_regression_model.pkl', 'rb'))
+# here we load random forest model as it gave good results when compared
+loaded_model=pickle.load(open('pickle-files/RandomForestRegressor.pkl', 'rb'))
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,7 +14,7 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    df=pd.read_csv('real_2018.csv')
+    df=pd.read_csv('Data/Real-Data/real_2018.csv')
     my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
     my_prediction=my_prediction.tolist()
     return render_template('result.html',prediction = my_prediction)
