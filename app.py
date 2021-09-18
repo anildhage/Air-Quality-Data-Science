f1734630583bf532c71ab55c60c7cfd522f59faf
anildhage <i.am.dhage@gmail.com>
2021-09-18 21:17:12 +0530

    simplified code for viewing on web

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,redirect
 import pandas as pd
-
+import glob
+import os
 import pickle
 
 # load the model from disk
@@ -8,16 +9,22 @@ import pickle
 loaded_model=pickle.load(open('pickle-files/RandomForestRegressor.pkl', 'rb'))
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods = ['POST', 'GET'])
 def home():
-	return render_template('home.html')
+   if request.method == 'POST':
+        year = request.form['value']
+        if len(year) == 0:
+            return render_template('home.html')
+        elif year != 2013 or year != 2014 or year != 2015 or year != 2016 or year != 2017 or year != 2018:
+            return render_template('home.html', num = 'You did not enter the above year correctly')
+        else:
+            df=pd.read_csv('Data/Real-Data/real_{}.csv'.format(year))
+            my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
+            my_prediction=my_prediction.tolist()
+            return render_template('home.html', prediction = my_prediction)
+   else:
+       return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    df=pd.read_csv('Data/Real-Data/real_2018.csv')
-    my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
-    my_prediction=my_prediction.tolist()
-    return render_template('result.html',prediction = my_prediction)
 
 
 
